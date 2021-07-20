16e133532e550e95cb9d9735c7eeb20836f85011
SvenRiebe <86112547+SvenRiebe@users.noreply.github.com>
2021-07-20 14:58:31 +0200

    Add files via upload

---
+++ DDM.ps1
@@ -0,0 +1,5 @@
+﻿Start-Process -FilePath ".\ddmsetup.exe" -ArgumentList "/Verysilent /NoUpdate"
+Start-Sleep -s 10
+Stop-process -Name ddm
+Set-ItemProperty -Path “HKCU:\Software\EnTech\DDM” LastCheck -Value 0 –Force
+Start-Process -FilePath "C:\Program Files (x86)\Dell\Dell Display Manager\ddm.exe"
